689e7ac89b071422c94790d8bf3472365ceb6fdb
Leonardo Freitas <leonardo.cadu2@hotmail.com>
2020-08-27 23:48:01 -0300

    add authors controller and view

---
+++ src/main/java/com/lfreitas/spring5demo/controllers/AuthorController.java
@@ -0,0 +1,24 @@
+package com.lfreitas.spring5demo.controllers;
+
+import com.lfreitas.spring5demo.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
+
+}
